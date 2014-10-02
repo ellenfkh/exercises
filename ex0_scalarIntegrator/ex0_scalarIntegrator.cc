d14ad14df8f65103b27e2cde780c6cde66af9610
Alex Gruver <agruver@g.hmc.edu>
2014-10-02 10:08:56 -0700

    cuda code was in the wrong files, whoops

--- ex0_scalarIntegrator/ex0_scalarIntegrator.cc
+++ ex0_scalarIntegrator/ex0_scalarIntegrator.cc
@@ -33,26 +33,6 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration;
 using std::chrono::duration_cast;
 
-__global__ void sum(double *toSum, unsigned int n)
-{
-  for(unsigned int i = 1; i < n; ++i)
-  {
-    toSum[0] += toSum[i];
-  }
-}
-
-__global__ void sumSection(double firstBound, unsigned long chunkSize,
-                    double* partial, unsigned long numberOfIntervals, double dx)
-{
-      int id = threadId.x;
-      unsigned long threadMax = std::min(numberOfIntervals, (id+1)*chunkPerThread);
-      for(unsigned long i = id*chunkPerThread; i < threadMax; ++i) {
-          const double evaluationPoint = bounds[0] + (double(i) + 0.5) * dx;
-          partialResults[id] += std::sin(evaluationPoint);
-      }
-      partialResults[id] *= dx;
-}
-
 class TbbFunctor {
 public:
 
--- ex0_scalarIntegrator/ex0_scalarIntegrator_cuda.cuh
+++ ex0_scalarIntegrator/ex0_scalarIntegrator_cuda.cuh
@@ -2,23 +2,5 @@
 #ifndef EX0_SCALARINTEGRATOR_CUDA_CUH
 #define EX0_SCALARINTEGRATOR_CUDA_CUH
 
-__global__ void sum(double *toSum, unsigned int n)
-{
-  for(unsigned int i = 1; i < n; ++i)
-  {
-    toSum[0] += toSum[i];
-  }
-}
 
-__global__ void sumSection(double firstBound, unsigned long chunkSize,
-                    double* partial, unsigned long numberOfIntervals, double dx)
-{
-      int id = threadId.x;
-      unsigned long threadMax = std::min(numberOfIntervals, (id+1)*chunkPerThread);
-      for(unsigned long i = id*chunkPerThread; i < threadMax; ++i) {
-          const double evaluationPoint = bounds[0] + (double(i) + 0.5) * dx;
-          partialResults[id] += std::sin(evaluationPoint);
-      }
-      partialResults[id] *= dx;
-}
 #endif // EX0_SCALARINTEGRATOR_CUDA_CUH
