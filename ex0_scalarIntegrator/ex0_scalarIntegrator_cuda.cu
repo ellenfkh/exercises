280734422ee558b7d7e5712f3878c1fe02ab16fc
Alex Gruver <agruver@g.hmc.edu>
2014-10-02 10:07:51 -0700

    cuda code was in the wrong files, whoops

--- ex0_scalarIntegrator/ex0_scalarIntegrator_cuda.cu
+++ ex0_scalarIntegrator/ex0_scalarIntegrator_cuda.cu
@@ -9,6 +9,26 @@
 
 __constant__ double constantPuppies[MAX_NUMBER_OF_PUPPIES];
 
+__global__ void sum(double *toSum, unsigned int n)
+{
+  for(unsigned int i = 1; i < n; ++i)
+  {
+    toSum[0] += toSum[i];
+  }
+}
+
+__global__ void sumSection(double firstBound, unsigned long chunkSize,
+                    double* partial, unsigned long numberOfIntervals, double dx)
+{
+      int id = threadId.x;
+      unsigned long threadMax = std::min(numberOfIntervals, (id+1)*chunkPerThread);
+      for(unsigned long i = id*chunkPerThread; i < threadMax; ++i) {
+          const double evaluationPoint = bounds[0] + (double(i) + 0.5) * dx;
+          partialResults[id] += std::sin(evaluationPoint);
+      }
+      partialResults[id] *= dx;
+}
+
 __global__
 void
 cudaDoScalarIntegration_kernel(double* output) {
